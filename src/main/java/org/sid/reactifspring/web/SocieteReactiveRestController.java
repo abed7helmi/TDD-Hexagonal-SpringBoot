90632066b474cdd535323436a77939dcb38660ff
abed7helmi <abed.helmi@yahoo.fr>
2023-11-23 13:19:29 +0100

    Reactive RestApi for Societe

--- src/main/java/org/sid/reactifspring/web/SocieteReactiveRestController.java
+++ src/main/java/org/sid/reactifspring/web/SocieteReactiveRestController.java
@@ -1,4 +1,36 @@
 package org.sid.reactifspring.web;
 
+import org.sid.reactifspring.dao.SocieteRepository;
+import org.sid.reactifspring.entities.Societe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
 public class SocieteReactiveRestController {
+    @Autowired
+    private SocieteRepository societeRepository;
+    @GetMapping(value = "/societes")
+    public Flux<Societe> findAll(){
+        return societeRepository.findAll();
+    }
+    @GetMapping(value = "/societes/{id}")
+    public Mono<Societe> getOne(@PathVariable String id){
+        return societeRepository.findById(id);
+    }
+    @PostMapping("/societes")
+    public Mono<Societe> save(@RequestBody Societe societe){
+        return societeRepository.save(societe);
+    }
+    @DeleteMapping(value = "/societes/{id}")
+    public Mono<Void> delete(@PathVariable String id){
+        return societeRepository.deleteById(id);
+    }
+    @PutMapping("/societes/{id}")
+    public Mono<Societe> update(@RequestBody Societe societe, @PathVariable String id){
+        societe.setId(id);
+        return societeRepository.save(societe);
+    }
+
 }
